7b6a13e90baed3cbfd054cf1adb8825101908aea
Enayat7800 <153251094+Enayat7800@users.noreply.github.com>
2024-02-18 17:23:26 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,19 +2,19 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6705357378:AAEFDCt40Hd1VziS4vSjnW1ZB1FHR6VX4LM"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "Filestolinkmyfastbot"
 
-    API_ID = ""
+    API_ID = "28150346"
 
-    API_HASH = ""
+    API_HASH = "426f0d0a1da02dea8fb71cb0bd3ab7e1"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "531353971583-g4sbdtil1oisn58bia4h3mp8gcr5g9oe.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [1251962299]
 
     VIDEO_DESCRIPTION = ""
 
