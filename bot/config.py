b043ac1f899888b386f3af5eac949d544cc7161b
Enayat7800 <153251094+Enayat7800@users.noreply.github.com>
2024-02-24 10:34:39 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,17 +2,17 @@ import os
 
 class Config:
 
-    BOT_TOKEN = "6705357378:AAEFDCt40Hd1VziS4vSjnW1ZB1FHR6VX4LM"
+    BOT_TOKEN = ""
 
-    SESSION_NAME = "Filestolinkmyfastbot"
+    SESSION_NAME = ""
 
     API_ID = "28150346"
 
     API_HASH = "426f0d0a1da02dea8fb71cb0bd3ab7e1"
 
-    CLIENT_ID = "531353971583-g4sbdtil1oisn58bia4h3mp8gcr5g9oe.apps.googleusercontent.com"
+    CLIENT_ID = "531353971583-e8ap75lq8l9f9l2tebv6ghmr448pmvcb.apps.googleusercontent.com"
 
-    CLIENT_SECRET = "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"
+    CLIENT_SECRET = "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"
 
     AUTH_USERS = [1251962299]
 
